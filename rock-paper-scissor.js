3418dca73287135548c2c3acb046d0e0909e3b6a
JorisLespagnol <46922441+Bwerpophile@users.noreply.github.com.>
2024-12-06 11:15:53 +0100

    Ready to start coding the core systeme of rock paper scissor.js

---
+++ rock-paper-scissor.js
@@ -0,0 +1,27 @@
+function getComputerChoice() {
+    return "rock";
+}
+
+console.log(getComputerChoice())
+
+function getHumanChoice(){
+    return prompt("Your choice");
+}
+
+
+getHumanChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+function playRound(humanChoice, computerChoice){
+    humanChoise.toLowerCase();
+    console.log('You lose Paper beats Rock')
+}
+
+const humanSelection= getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection,computerSelection)
