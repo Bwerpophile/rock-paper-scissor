3d4b0788f50885f055405a3f6add8ce2fe80e60d
JorisLespagnol <46922441+Bwerpophile@users.noreply.github.com.>
2024-12-06 14:47:29 +0100

    Jeu presque prêt il faut maintenant trouver comment ajouter l'interaction dans le code ainsi que vérifier que l'ordi selectionne bien de manière aléatoire

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,27 +1,68 @@
-function getComputerChoice() {
-    return "rock";
-}
-
-console.log(getComputerChoice())
-
-function getHumanChoice(){
-    return prompt("Your choice");
-}
-
-
-getHumanChoice();
 
 let humanScore = 0;
 let computerScore = 0;
 
-
-
-function playRound(humanChoice, computerChoice){
-    humanChoise.toLowerCase();
-    console.log('You lose Paper beats Rock')
+function getComputerChoice(i) {
+    i = Math.random(1);
+    console.log(i);
+    if (i < 1/3)  {
+        return computerChoice = "rock"
+    }
+    if (i < 2/3 )  {
+        return computerChoice = "paper"
+    }
+    else  {
+        return computerChoice = "scissor";
+    };
+}
+function getHumanChoice(){
+    humanChoice = prompt("Your choice");
+    humanChoice.toLowerCase(); 
+    return humanChoice;   
 }
 
-const humanSelection= getHumanChoice();
-const computerSelection = getComputerChoice();
+function playRound(humanChoice, computerChoice){
+    
+    
+        if (humanChoice == "rock" && computerChoice == "scissor") {
+            humanScore = humanScore + 1;
+            console.log("Choix ordi" + " " + computerChoice + "Choix humain" + " " +humanChoice)
+            console.log("Computer : " + computerScore + " | " + "Human : " + humanScore)
+            return console.log(humanScore) + console.log("Rock beat scissor") ;
+        }
+        if (humanChoice == "paper" && computerChoice == "rock") {
+            humanScore = humanScore + 1 ;
+            console.log("Choix ordi" + " " + computerChoice +" "+  "Choix humain" + " " +humanChoice)
+            console.log("Computer : " + computerScore + " | " + "Human : " + humanScore)
+            return console.log(humanScore) + console.log("Paper beat rock") ;
+        } 
+        if (humanChoice == "scissor" && computerChoice == "paper") {
+            humanScore = humanScore + 1;
+            console.log("Choix ordi" + " " + computerChoice +" "+  "Choix humain" + " " +humanChoice)
+            console.log("Computer : " + computerScore + " | " + "Human : " + humanScore)
+            return console.log(humanScore) + console.log("scissor beat paper") ;
+            
+        }
+        if (humanChoice === computerChoice) {
+            console.log("Choix ordi" + " " + computerChoice +" "+  "Choix humain" + " " +humanChoice)
+            console.log("Computer : " + computerScore + " | " + "Human : " + humanScore)
+            return console.log(humanScore) + console.log("That's a draw") ;
+            
+        }
+        else {
+            computerScore = computerScore + 1;
+            console.log('computer wins')
+            console.log("Choix ordi" + " " + computerChoice + " "+ "Choix humain" + " " +humanChoice)
+            console.log("Computer : " + computerScore + " | " + "Human : " + humanScore)
+        }      
+    
+};
 
-playRound(humanSelection,computerSelection)
+const humanSelection= getHumanChoice();
+const computerSelection = getComputerChoice(); 
+
+for (let i = 0; i < 5; i++) {
+  
+    
+    playRound(humanSelection, computerSelection); 
+}
